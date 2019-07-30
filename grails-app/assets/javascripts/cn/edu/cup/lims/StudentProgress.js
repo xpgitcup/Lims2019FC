d67a78b9af788aa5c982b4cfac47a46cf70a1f45
XIAOMI-CHUYUN52\LiXiaoPing <xpli126@126.com>
2019-07-30 20:38:54 +0800

    大连的工作

---
+++ grails-app/assets/javascripts/cn/edu/cup/lims/StudentProgress.js
@@ -0,0 +1,75 @@
+bootStrapPaginationSetting.identifier = "Student"
+bootStrapPaginationSetting.controller = "operation4Student"
+bootStrapPaginationSetting.appendFunction = "appendParamsBootStrapStudent";
+
+var card进度维护Div;
+
+$(function () {
+    console.info(document.title + "加载了...")
+    card进度维护Div = $("#card进度维护Div");
+    setupPagination4Card(card进度维护Div);
+    loadCurrentPageBootStrap("进度维护");
+
+    var currentId = $("#currentId").text().trim();
+    $("#createProgress").attr("href", "/operation4Teacher/create?team=" + currentId);
+
+})
+
+/*
+* 生成附加参数---针对每个页面都要重新定义！！！
+* */
+function appendParamsBootStrap(title) {
+    var append = ""
+
+    /*
+    * 适应多分支的index函数
+    * */
+    var currentStatus = $("#currentStatus").text().trim()
+    if (currentStatus != undefined) {
+        append += "&currentStatus=" + currentStatus;
+
+        var currentId = $("#currentId").text().trim();
+        if (currentId != undefined) {
+            if (parseInt(currentId) > 0) {
+                append += "&currentId=" + currentId;
+            }
+        }
+    }
+
+    var filter = readStorage("filter" + document.title, "false");
+    var keyString = readStorage("keyString" + document.title, "");
+
+    if (filter != "false") {
+        // 更新显示
+        $("#currentFilter").html(keyString)
+        append = "&like=" + keyString;
+    }
+    return append;
+}
+
+/*
+* 查询--需要各个页面自定义
+* */
+function queryStatementBootStrap() {
+    // 获取数据
+    var keyString = document.getElementById("keyString");
+
+    console.log("查询..." + keyString.value);
+    console.info("查询..." + keyString.value);
+
+    // 保存数据
+    sessionStorage.setItem("filter" + document.title, "true");
+    sessionStorage.setItem("keyString" + document.title, keyString.value);
+
+
+    location.reload();
+}
+
+/*
+* 清除过滤条件
+* */
+function clearFilterBootStrap() {
+    sessionStorage.setItem("filter" + document.title, false)
+    $("#currentFilter").html("")
+    location.reload();
+}
--- grails-app/views/operation4QueryStatementA/_listQueryStatementA.gsp
+++ grails-app/views/operation4QueryStatementA/_listQueryStatementA.gsp
@@ -3,8 +3,8 @@
 <table class="table table-striped table-sm m-0 p-0">
     <thead>
     <th width="7%">id</th>
-    <th width="10%">Controller</th>
-    <th width="7%">关键字</th>
+    <th width="12%">Controller</th>
+    <th width="10%">关键字</th>
     <th width="10%">参数</th>
     <th>Action</th>
     <th width="45%">查询</th>
